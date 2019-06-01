3f2e0d8128a3cd51e9f0d24edf4134dd1b423f70
thanga91 <thanga91@gmail.com>
2019-06-01 18:44:58 +0530

    Initial project setup

---
+++ TaskManagerApi/Controllers/TaskManagerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskManagerController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            return Ok("Get All Tasks");
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddTask()
+        {
+            return Ok("Task Added");
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateTask()
+        {
+            return Ok("Task updated");
+        }
+
+        [HttpDelete("")]
+
+        public async Task<IActionResult> DeleteTask()
+        {
+            return Ok("Deleted Task");
+        }
+    }
+}
