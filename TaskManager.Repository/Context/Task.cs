79075a1870932f80cbf496c4fb9be7e49d245c87
thanga91 <thanga91@gmail.com>
2019-06-01 21:16:57 +0530

    Created Database

---
+++ TaskManager.Repository/Context/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Repository.Context
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public int ParentId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Priority { get; set; }
+    }
+}
---
+++ TaskManager.Repository/Context/TaskDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Repository.Context
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Task> Employees { get; set; }
+    }
+}
